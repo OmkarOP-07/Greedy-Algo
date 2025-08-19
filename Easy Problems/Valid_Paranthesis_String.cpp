5a39bb87d3500b54e0e80e11f8148d6d25cdb044
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-08-19 23:59:02 +0530

    Check Paranthesis

--- Easy Problems/Valid_Paranthesis_String.cpp
+++ Easy Problems/Valid_Paranthesis_String.cpp
@@ -2,7 +2,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int min = 0;
+        int max = 0;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '('){
+                min++;
+                max++;
+            } else if(s[i] == ')'){
+                min--;
+                max--;
+            } else {
+                min = min - 1;
+                max = max + 1;
+            }
+            if(min < 0) min = 0;
+            if(max < min ) return false;
+        }
+        return (min == 0);
+    }
+};
+
 int main()
 {
+    string str = "(*)";
+    Solution sh;
+    cout<<sh.checkValidString(str);
+
  return 0;
 }
